de13558613cb8c754887afbafa38bca72318511e
mbalcer <mateusz75319@gmail.com>
2019-05-23 20:01:17 +0200

    Added controller

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import annotation.Scheduled;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainController {
+    private Object object = null;
+
+    public void startMethod(Method method, Object object) {
+        Timer timer = new Timer();
+        Scheduled scheduled = method.getAnnotation(Scheduled.class);
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    method.invoke(object);
+                } catch (ReflectiveOperationException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, scheduled.period());
+    }
+
+    public Class<?> searchClass(String nameClass) {
+        try {
+            return Class.forName(nameClass);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+
+    public void createObject(Class<?> nameClass) {
+        try {
+            object = nameClass.newInstance();
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+        }
+
+        Method[] methods = nameClass.getDeclaredMethods();
+        Arrays.stream(methods)
+              .filter(method -> method.isAnnotationPresent(Scheduled.class))
+              .forEach(method -> startMethod(method, object));
+    }
+}
